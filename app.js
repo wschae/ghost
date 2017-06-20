3e22fbf02d21f1796ae88799296b6797287b081a
Wonseok Chae <wochae@microsoft.com>
2017-06-20 15:29:40 -0700

    add docker run api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express();
 var bodyParser = require('body-parser');
+var Docker = require('dockerode');
 
 var port = process.env.PORT || 8080;
 
@@ -8,6 +9,14 @@ app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 // app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json    
+
+app.get('/docker/run', function(req, res){    
+    var docker = new Docker();
+    docker.run('ghost', [], [process.stdout, process.stderr], {Tty: false, env: ['VIRTUAL_HOST=demo3.localhost']} , function(error, data, container){
+        console.log(data.StatusCode);
+    });
+    res.send('hello docker');
+});
 
 app.post('/api/setup', function(req, res) {
     var data = req.body;
