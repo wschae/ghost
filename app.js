2b6b1ade8761577090cb50201cba6d56a82b426c
Wonseok Chae <wochae@microsoft.com>
2017-06-21 01:24:16 -0700

    v2

--- app.js
+++ app.js
@@ -2,26 +2,34 @@ var express = require('express')
 var app = express();
 var bodyParser = require('body-parser');
 var Docker = require('dockerode');
+var fs     = require('fs');
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json    
-
-app.get('/docker/run', function(req, res){    
-    var docker = new Docker();
-    docker.run('ghost', [], [process.stdout, process.stderr], {Tty: false, env: ['VIRTUAL_HOST=demo3.localhost']} , function(error, data, container){
-        console.log(data.StatusCode);
-    });
-    res.send('hello docker');
-});
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json    
 
 app.post('/api/setup', function(req, res) {
     var data = req.body;
     console.log('name: ' + data.name);
-    res.send({redirect: 'http://www.google.com/search?q='+data.name});
+        
+    var socket = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
+    var stats  = fs.statSync(socket);
+
+    if (!stats.isSocket()) {
+    throw new Error('Are you sure the docker is running?');
+    }    
+    var docker = new Docker({socketPath: socket});
+    docker.run('ghost', [], [process.stdout, process.stderr], {Tty: false, env: ['VIRTUAL_HOST='+data.name+'.localhost']} , function(error, data, container){
+        console.log(err);
+        res.send('Error');
+    });
+
+    // need to wait?
+    console.log("redorected to " + data.name+'.localhost/');
+    res.send({redirect: 'http://'+data.name+'.localhost/ghost/setup/one/'});
 });
 
 app.get('*', function(req, res) {
